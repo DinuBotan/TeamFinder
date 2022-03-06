e8cb5027bcfddac8d9e1d64add2b340eda8d470b
DinuBotan <dinubotan@gmail.com>
2022-03-07 01:21:42 +0200

    Persist messages from socket connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,39 +1,39 @@
-const path = require('path')
-const http = require('http')
-const express = require('express')
-const socketio = require('socket.io')
-require('./db/mongoose')
-const User = require('./models/user')
-const Team = require('./models/team')
-const userRouter = require('./routers/user')
-const teamRouter = require('./routers/team')
-const messageRouter = require('./routers/message')
-const chatRoomRouter = require('./routers/chatRoom')
-const bodyParser = require('body-parser')
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+require("./db/mongoose");
+const User = require("./models/user");
+const Team = require("./models/team");
+const userRouter = require("./routers/user");
+const teamRouter = require("./routers/team");
+const messageRouter = require("./routers/message");
+const chatRoomRouter = require("./routers/chatRoom");
+const bodyParser = require("body-parser");
 
-const app = express()
+const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json())
-app.use(userRouter)
-app.use(teamRouter)
-app.use(messageRouter)
-app.use(chatRoomRouter)
+app.use(bodyParser.json());
+app.use(userRouter);
+app.use(teamRouter);
+app.use(messageRouter);
+app.use(chatRoomRouter);
 
 // We need to create the server (which express does behind the scenes), because it is required for running socket.io
-const server = http.createServer(app)
-const io = socketio(server)
+const server = http.createServer(app);
+const io = socketio(server);
 
-const port = process.env.PORT || 3001
-const directoryPath = path.join(__dirname)
+const port = process.env.PORT || 3001;
+const directoryPath = path.join(__dirname);
 
-app.use(express.static(directoryPath)) 
+app.use(express.static(directoryPath));
 
 // The way to call an event that happens to our io connection
-io.on('connection', (socket) => {
-    require('./io')(socket, io)
-})
+io.on("connection", (socket) => {
+  require("./io")(socket, io, app);
+});
 
 server.listen(port, () => {
-    console.log('Server is up on port ' + port)
-    console.log(directoryPath)
-})
+  console.log("Server is up on port " + port);
+  console.log(directoryPath);
+});
--- server/src/io.js
+++ server/src/io.js
@@ -1,35 +1,46 @@
-module.exports = function(socket, io) {
-    console.log('New WebSocket Connection')
+const Message = require("./models/message");
+module.exports = function (socket, io, app) {
+  console.log("New WebSocket Connection");
 
-    // Will send the message to everyone except the person sending the message
-    socket.broadcast.emit('message', 'A new user has joined!')
+  // Will send the message to everyone except the person sending the message
+  socket.broadcast.emit("message", "A new user has joined!");
 
-    socket.on('join', (user) => {
-        // socket.join(user)
-        var json = JSON.parse(user)
-        console.log('User joined: ', json.username)
-        console.log('In room: ', json.room)
+  socket.on("join", (user) => {
+    // socket.join(user)
+    var json = JSON.parse(user);
+    console.log("User joined: ", json.username);
+    console.log("In room: ", json.room);
 
-        // This only sends data to a specific client
-        socket.emit('message', 'Welcome to the chat')
+    // This only sends data to a specific client
+    socket.emit("message", "Welcome to the chat");
 
-        // socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`))
+    // socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`))
+  });
 
+  // We want to emit to every connection:
+  socket.on("sendMessage", (message) => {
+    io.emit("message", message);
+    console.log("Emitted message: ", message);
 
-    })
+    // Persist message to database
+    new Message({
+      author: "Dinu",
+      content: message,
+      timestamp: "12:00",
+      chatRoom: "Toom",
+    }).save((err) => {
+      if (err) {
+        return console.log("error");
+      }
+    });
+  });
 
-    // We want to emit to every connection:
-    socket.on('sendMessage', (message) => {
-        io.emit('message', message)
-        console.log('Emitted message: ', message)
-    })
+  socket.on("message", () => {
+    console.log("Received a message!");
+  });
 
-    socket.on('message', () => {
-        console.log('Received a message!')
-    })
-
-    socket.on('disconnect', () => {
-        io.emit('message', 'A user has left')
-        console.log("Disconnected!")
-    })
-}
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left");
+    console.log("Disconnected!");
+  });
+};
