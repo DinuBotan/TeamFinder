f84fe4ba0803a3ddd580f01cb92f82a27b575806
DinuBotan <dinubotan@gmail.com>
2022-04-20 18:35:14 +0200

    Add navigation bar to teams list

--- android/app/src/main/java/com/project/teamfinder/ui/teams/TeamsScreen.kt
+++ android/app/src/main/java/com/project/teamfinder/ui/teams/TeamsScreen.kt
@@ -1,20 +1,37 @@
 package com.project.teamfinder.ui.teams
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.outlined.Create
+import androidx.compose.material.icons.outlined.ExitToApp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.project.model.response.TeamResponse
 import com.project.teamfinder.ui.TeamsApplication
+import com.project.teamfinder.ui.conversation.ConversationContent
+import com.project.teamfinder.ui.team.TeamContent
+import com.project.teamfinder.ui.team.TeamViewModel
 import com.project.teamfinder.ui.theme.TeamFinderTheme
 
 @Composable
@@ -24,7 +41,25 @@ fun TeamsScreen(userId: String, navController: NavHostController) {
     viewModel.userId = userId
     val teams = viewModel.teamsState.value
 
-    LazyColumn(contentPadding = PaddingValues(16.dp)) {
+    Scaffold(topBar = {
+        AppBar(
+            title = "Teams list"
+        )
+    }) {
+        Surface(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {}
+        }
+    }
+
+    LazyColumn(
+//        contentPadding = PaddingValues(60.dp),
+        modifier = Modifier.absolutePadding(10.dp, 50.dp, 10.dp, 3.dp)
+    ) {
         items(teams) { team ->
             Team(team, viewModel, navController, userId) {
                 if(viewModel.belongsToTeam(team.id)) {
@@ -33,6 +68,24 @@ fun TeamsScreen(userId: String, navController: NavHostController) {
             }
         }
     }
+}
+
+@Composable
+fun AppBar(title: String) {
+    TopAppBar(
+        title = { Text(title) },
+        actions = {
+            Icon(
+                Icons.Outlined.Create,
+                contentDescription = "Create team",
+                modifier = Modifier
+                    .padding(horizontal = 12.dp)
+                    .clickable(onClick = {
+
+                    })
+            )
+        }
+    )
 }
 
 @Composable
